70c9233ac5cfc73a8090fe5f47a34000519beaea
luisfmatheuscorrea <luisfilipe.ac@gmail.com>
2021-04-22 00:16:06 -0300

    Consumindo JSON

---
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -0,0 +1,65 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
+import 'package:flutter/material.dart';
+
+class QuizCardWidget extends StatelessWidget {
+  final String title;
+  final String completed;
+  final double percent;
+
+  const QuizCardWidget({
+    Key? key,
+    required this.title,
+    required this.completed,
+    required this.percent,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      padding: EdgeInsets.all(12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 40,
+            width: 40,
+            child: Image.asset(AppImages.blocks),
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          Text(
+            title,
+            style: AppTextStyles.heading15,
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Text(
+                  completed,
+                  style: AppTextStyles.body11,
+                ),
+              ),
+              Expanded(
+                flex: 4,
+                child: ProgressIndicatorWidget(
+                  value: percent,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
